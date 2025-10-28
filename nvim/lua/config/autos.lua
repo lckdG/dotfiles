93e4371a27afeb9cec9fc7190b63be79892c8033
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-10-29 01:18:45 +0700

    * Disable clear buffer QuitPre for now

--- nvim/lua/config/autos.lua
+++ nvim/lua/config/autos.lua
@@ -6,11 +6,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd('QuitPre', {
-    desc = 'Clear buffers when exiting Vim',
-    group = vim.api.nvim_create_augroup('exit-clear-buffers', { clear = true }),
-    callback = function ()
-        require('close_buffers').delete( { type = 'hidden' })
-    end
-})
+-- vim.api.nvim_create_autocmd('QuitPre', {
+--     desc = 'Clear buffers when exiting Vim',
+--     group = vim.api.nvim_create_augroup('exit-clear-buffers', { clear = true }),
+--     callback = function ()
+--         require('close_buffers').delete( { type = 'hidden' })
+--     end
+-- })
 
