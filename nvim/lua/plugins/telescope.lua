c5244e1b09a095103d82b027118eea3e10b842ff
luckydoG_ <vkthanh1999@gmail.com>
2025-10-28 14:45:13 +0700

    + Add telescope-fzf-native

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,4 +6,7 @@ return {
         { 'nvim-telescope/telescope-ui-select.nvim' },
         { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font }
     },
+    config = function ()
+        require('telescope').load_extension('fzf')
+    end
 }
