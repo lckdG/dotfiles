d01911c489f1deb4275ea2d6fbf30eaba983f59d
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-10-29 22:14:59 +0700

    * Improve lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -74,15 +74,21 @@ return {
         vim.diagnostic.config {
             severity_sort = true,
             float = { border = 'rounded', source = 'if_many' },
-            underline = { severity = vim.diagnostic.severity.ERROR },
-            signs = vim.g.have_nerd_font and {
-            text = {
-                [vim.diagnostic.severity.ERROR] = '󰅚 ',
-                [vim.diagnostic.severity.WARN] = '󰀪 ',
-                [vim.diagnostic.severity.INFO] = '󰋽 ',
-                [vim.diagnostic.severity.HINT] = '󰌶 ',
+            underline = {
+                        severity = {
+                            vim.diagnostic.severity.ERROR,
+                            vim.diagnostic.severity.WARN,
+                            vim.diagnostic.severity.HINT
+                        }
+                    },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = '󰅚 ',
+                    [vim.diagnostic.severity.WARN] = '󰀪 ',
+                    [vim.diagnostic.severity.INFO] = '󰋽 ',
+                    [vim.diagnostic.severity.HINT] = '󰌶 ',
+                },
             },
-            } or {},
             virtual_text = false,
         }
 
