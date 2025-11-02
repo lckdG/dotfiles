07214c242d6c87289d2a1ac96e0f9305c7a25981
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-11-02 12:58:40 +0700

    * Back to default harpoon UI

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -95,24 +95,7 @@ map('n', '<leader>bdw', function ()
 -- Harpoon
 local harpoon = require('harpoon')
 
-local conf = require('telescope.config').values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require('telescope.pickers').new({}, {
-        prompt_title = "Harpoon",
-        finder = require('telescope.finders').new_table({
-            results = file_paths
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
-map('n', '<leader>he', function () toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+map('n', '<leader>he', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon window" })
 map('n', '<leader>ha', function() harpoon:list():add() end, { desc = "Add file to harpoon" })
 
 map('n', '<leader>h1', function () harpoon:list():select(1) end, { desc = "Jump to 1st file in harpoon list" })
