3a5bbe3ec30b2864ee473a3f4f7e1172fadd2fef
luckydoG_ <vkthanh1999@gmail.com>
2025-11-03 12:54:31 +0700

    * Configurations for oil.nvim

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,9 +7,17 @@ return {
         columns = {
             "icon",
             "size",
-            "mtime",
+            -- "mtime",
         },
         delete_to_trash = true,
+        watch_for_changes = true,
+        view_options = {
+            natural_order = true,
+        },
+        float = {
+            border = "rounded",
+            preview_split = "above",
+        }
     },
     dependencies = {
         'nvim-tree/nvim-web-devicons'
