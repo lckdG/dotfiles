0caaa7f7fc9c563fe0655ad058ea84f30ae62043
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-11-05 00:29:15 +0700

    * Correctly use fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -32,6 +32,14 @@ return {
                 },
                 file_ignore_patterns = ignore_patterns,
             },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+            },
         })
 
         telescope.load_extension('fzf')
