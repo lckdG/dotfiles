d1dc9b92b1a5b11ecc3efc26937c2928ca6c4cac
luckydoG_ <vkthanh1999@gmail.com>
2025-11-06 15:49:24 +0700

    * Compiler diagnostic to fullSolution

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -111,7 +111,7 @@ return {
                 settings = {
                     ["csharp|background_analysis"] = {
                         dotnet_analyzer_diagnostics_scope = "openFiles",
-                        dotnet_compiler_diagnostics_scope = "openFiles",
+                        dotnet_compiler_diagnostics_scope = "fullSolution",
                     },
                 },
             },
