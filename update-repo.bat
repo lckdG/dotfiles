e996c817780731c379ebe89e8f7f29f531d0e2d9
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-11-12 18:01:34 +0700

    feat(sync): handle syncing wezterm

--- update-repo.bat
+++ update-repo.bat
@@ -1,5 +1,13 @@
 @echo off
 robocopy %UserProfile%\AppData\Local\nvim nvim /mir
 
+if exist %UserProfile%\.wezterm.lua (
+    if not exist wezterm mkdir wezterm
+    copy %UserProfile%\.wezterm.lua wezterm\.wezterm.lua
+    echo Sync .wezterm.lua completed
+) else (
+    echo No .wezterm.lua file found, skip wezterm sync
+)
+
 PAUSE
 
---
+++ update-wezterm.bat
@@ -0,0 +1,7 @@
+@echo off
+
+copy wezterm\.wezterm.lua %UserProfile%\.wezterm.lua
+echo Copy .wezterm.lua completed
+
+PAUSE
+
