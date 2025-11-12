ace485608bc98efce9ce398873870938727de46f
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-11-12 16:42:56 +0700

    perf: reduce startup time

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,43 +1,43 @@
 return {
     'saghen/blink.cmp',
-    event = 'VimEnter',
+    event = { "BufReadPost", "BufNewFile" },
     version = '1.*',
     build = 'cargo build --release',
     dependencies = {
-    {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-        end
-        return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        {
+            'L3MON4D3/LuaSnip',
+            version = '2.*',
+            build = (function()
+                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                    return
+                end
+                return 'make install_jsregexp'
+            end)(),
+            dependencies = {
+                -- `friendly-snippets` contains a variety of premade snippets.
+                --    See the README about individual language/framework/plugin snippets:
+                --    https://github.com/rafamadriz/friendly-snippets
+                -- {
+                --   'rafamadriz/friendly-snippets',
+                --   config = function()
+                --     require('luasnip.loaders.from_vscode').lazy_load()
+                --   end,
+                -- },
+            },
+            opts = {},
         },
-        opts = {},
-    },
-    'folke/lazydev.nvim',
+        'folke/lazydev.nvim',
     },
     opts = {
-    keymap = {
-        preset = 'default',
-        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-        ["<Esc>"] = { "hide", "fallback" },
-        ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
-        ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
-    }
+        keymap = {
+            preset = 'default',
+            ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+            ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Esc>"] = { "hide", "fallback" },
+            ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+            ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+        }
     },
     appearance = {
         nerd_font_variant = 'mono',
