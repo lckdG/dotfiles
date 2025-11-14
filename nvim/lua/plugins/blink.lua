ba5991b9b40d8200f08be3154fc11fedb40f3890
Thanh Vo Khac <vkthanh1999@gmail.com>
2025-11-14 20:49:37 +0700

    feat(completion): improve blink.cmp

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,13 +1,35 @@
 return {
     'saghen/blink.cmp',
-    event = "InsertEnter",
+    event = "VeryLazy",
     version = '1.*',
     build = 'cargo build --release',
     dependencies = {
-        'folke/lazydev.nvim',
+        { 'folke/lazydev.nvim' },
+        { 
+            'L3MON4D3/LuaSnip',
+            version = '2.*',
+            build = (function ()
+                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                    return
+                end
+
+                return 'make install_jsregexp'
+            end)(),
+            dependencies = {},
+            opts = {},
+        }
     },
-    opts = {
-        keymap = {
+    opts = function(_, opts)
+        opts.enabled = function ()
+            local filetype = vim.bo[0].filetype
+            if filetype == "TelescopePrompt" or filetype == "minifiles" or filetype == "snacks_picker_input" then
+                return false
+            end
+
+            return true
+        end
+
+        opts.keymap = {
             preset = 'default',
             ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
             ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
@@ -16,20 +38,24 @@ return {
             ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
             ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
         }
-    },
-    appearance = {
-        nerd_font_variant = 'mono',
-    },
-    completion = {
-        documentation = { auto_show = false, auto_show_delay_ms = 250 },
-    },
-    sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
-        providers = {
-            lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-    },
-    snippets = { preset = 'luasnip' },
-    fuzzy = { implementation = 'lua' },
-    signature = { enabled = true },
+
+        opts.completion = {
+            menu = {
+                border = "single"
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 250
+            },
+        }
+
+        opts.sources = {
+            default = { 'lsp', 'path', 'snippets', 'lazydev' },
+            providers = {
+                lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+            },
+        }
+        opts.snippets = { preset = 'luasnip' }
+        opts.signature = { enabled = true }
+    end,
 }
