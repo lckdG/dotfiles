7625048a8ae7f1fec7bf2191d6d5b0250a2888a1
luckydoG_ <vkthanh1999@gmail.com>
2025-11-17 15:10:09 +0700

    feat: use the new tree-sitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,23 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
+    branch = 'main',
     lazy = false,
     build = ":TSUpdate",
-    main = 'nvim-treesitter.configs',
-    opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function ()
+        require("nvim-treesitter").install {
+            'bash',
+            'c',
+            'diff',
+            'html',
+            'lua',
+            'luadoc',
+            'markdown',
+            'markdown_inline',
+            'query',
+            'vim',
+            'vimdoc',
+            'c_sharp',
+            'cpp',
+        }
+    end
 }
