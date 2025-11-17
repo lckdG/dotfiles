c9c1a17317b39e764ff1521d7cfcb444ccbc3401
luckydoG_ <vkthanh1999@gmail.com>
2025-11-17 15:10:20 +0700

    feat(init): update init-scoop

--- init-scoop.bat
+++ init-scoop.bat
@@ -6,14 +6,17 @@ CALL scoop install make llvm rustup
 CALL scoop install ripgrep fd
 CALL scoop install neovim
 
+REM tree sitter setup
+CALL cargo install --locked tree-sitter-cli
+
+REM Install lua language server
+CALL scoop install lua-language-server
+
 REM Unrem these if you want to use neovide
 REM CALL scoop install neovide
 
-REM Unrem these if you want to use wezterm
-REM CALL scoop install wezterm
-
-REM Unrem these for starship use
-REM CALL scoop install starship
+REM Unrem these if you want to use wezterm & starship
+REM CALL scoop install wezterm starship
 
 CALL scoop update *
 
