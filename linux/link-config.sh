ad76ad578035a0de23a0ef10000103a341424aa8
luckydoG <vkthanh1999@gmail.com>
2025-12-15 22:37:33 +0700

    fix(linux): correct link-config

--- linux/link-config.sh
+++ linux/link-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ..
 # Links the configs to /home/.config directory, in case of conflict, try --adopt to override
-stow -S ../config -t $HOME/.config -v
+stow -S config -t $HOME/.config -v
 
