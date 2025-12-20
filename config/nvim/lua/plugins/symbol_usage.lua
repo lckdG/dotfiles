d3f0c99664e05018e668436104516f68b5f420b1
luckydoG <vkthanh1999@gmail.com>
2025-12-20 17:06:58 +0700

    feat(neovim): add lens kinds for cs

--- config/nvim/lua/plugins/symbol_usage.lua
+++ config/nvim/lua/plugins/symbol_usage.lua
@@ -1,9 +1,19 @@
+local SymbolKind = vim.lsp.protocol.SymbolKind
+
 return {
     'Wansmer/symbol-usage.nvim',
     event = 'BufReadPre',
     config = function ()
         require('symbol-usage').setup({
-            
+            filetypes = {
+                cs = {
+                    kinds = {
+                        SymbolKind.Method,
+                        SymbolKind.Event,
+                        SymbolKind.Property,
+                    },
+                },
+            }
         })
     end
 }
