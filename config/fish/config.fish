90974a10fee6e69c1482bac589a1cc23dff7bd3b
luckydoG <vkthanh1999@gmail.com>
2025-12-22 22:34:31 +0700

    feat(fish): abbreviation to run commands in background

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,10 +11,14 @@ if status is-interactive
     fish_add_path $HOME/.local/share/gem/ruby/3.4.0/bin
     fish_add_path $HOME/Apps/Roslyn/content/LanguageServer/linux-x64/
 
+    # Abbreviations
+
     abbr ff fastfetch
     abbr lg lazygit
 
     abbr sdh shutdown -h
     abbr sdr shutdown -r
+
+    abbr -a --position anywhere -- --bg "> /dev/null 2>&1 &"
 end
 
