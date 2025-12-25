6a996a9cd50ca57109a3f828cfb9e56459178c2c
luckydoG <vkthanh1999@gmail.com>
2025-12-25 16:12:53 +0700

    feat(windows): create symlink for lazygit

--- windows/create-symlinks.bat
+++ windows/create-symlinks.bat
@@ -2,8 +2,10 @@
 
 call rmdir %LOCALAPPDATA%\nvim
 call del %USERPROFILE%\.wezterm.lua
+call rmdir %LOCALAPPDATA%\lazygit
 
 call mklink /D %LOCALAPPDATA%\nvim %~dp0..\config\nvim
 call mklink %USERPROFILE%\.wezterm.lua %~dp0..\config\wezterm\wezterm.lua
+call mklink /D %LOCALAPPDATA%\lazygit %~dp0..\config\lazygit
 
 pause
