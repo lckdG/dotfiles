1111cdf096d5b1b910432843344c6587a23eef5d
luckydoG_ <vkthanh1999@gmail.com>
2026-01-13 11:10:26 +0700

    feat(wezterm): try with 11pt font size

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -102,7 +102,7 @@ config.inactive_pane_hsb = {
 }
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 12
+config.font_size = 11
 
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 
