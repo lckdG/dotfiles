14c2566b9dac8b5708606407773ae482583ff34c
luckydoG <vkthanh1999@gmail.com>
2026-01-24 23:17:42 +0700

    refactor(wezterm): more specialized keybindings

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -238,6 +238,7 @@ config.launch_menu = launch_menu
 
 --------------- Key Bindings ---------------
 
+config.disable_default_key_bindings = true
 config.mouse_bindings = {
     {
         event = { Down = { streak = 1, button = { WheelUp = 1 } } },
@@ -257,30 +258,45 @@ local act = wezterm.action
 
 config.leader = { key = "b", mods = "CTRL", timeout_milisecond = 2000 }
 config.keys = {
-    { key = "b", mods = "LEADER|CTRL", action = act({ SendString = "\x02" })},
+    { key = "b", mods = "LEADER|CTRL", action = act.SendString "\x02" },
+    { key = "r", mods = "LEADER", action = act.ReloadConfiguration },
     { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
 
-    { key = "-", mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-    { key = "|", mods = "LEADER|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+    { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo "Clipboard" },
+    { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom "Clipboard" },
+
+    { key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+    { key = "+", mods = "CTRL|SHIFT", action = act.IncreaseFontSize },
+    { key = "0", mods = "CTRL", action = act.ResetFontSize },
+
+    { key = "]", mods = "CTRL", action = act.ActivateTabRelative(1) },
+    { key = "[", mods = "CTRL", action = act.ActivateTabRelative(-1) },
+    { key = "}", mods = "CTRL|SHIFT", action = act.MoveTabRelative(1) },
+    { key = "{", mods = "CTRL|SHIFT", action = act.MoveTabRelative(-1) },
+    { key = "]", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(1)},
+    { key = "[", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(-1)},
+
+    { key = "l", mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
+    { key = "f", mods = "CTRL|SHIFT", action = act.Search { CaseSensitiveString="" }},
+
+    { key = "-", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+    { key = "|", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
     { key = "n", mods = "LEADER", action = act.ShowLauncherArgs { flags = "LAUNCH_MENU_ITEMS" } },
 
-    { key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
-    { key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
-    { key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
-    { key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
-    { key = "LeftArrow", mods = "LEADER|CTRL", action = act({ AdjustPaneSize = { "Left", 5 } }) },
-    { key = "DownArrow", mods = "LEADER|CTRL", action = act({ AdjustPaneSize = { "Down", 5 } }) },
-    { key = "UpArrow", mods = "LEADER|CTRL", action = act({ AdjustPaneSize = { "Up", 5 } }) },
-    { key = "RightArrow", mods = "LEADER|CTRL", action = act({ AdjustPaneSize = { "Right", 5 } }) },
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection "Left" },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection "Down" },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection "Up" },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection "Right" },
+    { key = "LeftArrow", mods = "LEADER|CTRL", action = act.AdjustPaneSize { "Left", 5 } },
+    { key = "DownArrow", mods = "LEADER|CTRL", action = act.AdjustPaneSize { "Down", 5 } },
+    { key = "UpArrow", mods = "LEADER|CTRL", action = act.AdjustPaneSize { "Up", 5 } },
+    { key = "RightArrow", mods = "LEADER|CTRL", action = act.AdjustPaneSize { "Right", 5 } },
 
     { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
     { key = "f", mods = "LEADER", action = act.ToggleFullScreen },
 
-    { key = "d", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
-    { key = "x", mods = "LEADER", action = act({ CloseCurrentTab = { confirm = true } }) },
-
-    { key = "n", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(1)},
-    { key = "p", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(-1)},
+    { key = "d", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+    { key = "x", mods = "LEADER", action = act.CloseCurrentTab { confirm = true } },
 
     { key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
     { key = "w", mods = "LEADER|SHIFT", action = act.PromptInputLine {
