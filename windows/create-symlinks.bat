03ffdaa7eeda81f924f116ab517bf57b3010c8db
luckydoG_ <vkthanh1999@gmail.com>
2026-01-26 10:14:37 +0700

    refactor: create symlink to wezterm folder

--- windows/create-symlinks.bat
+++ windows/create-symlinks.bat
@@ -1,11 +1,13 @@
 @echo off
 
+if not exist %USERPROFILE%\.config mkdir %USERPROFILE%\.config
+
 call rmdir %LOCALAPPDATA%\nvim
-call del %USERPROFILE%\.wezterm.lua
+call rmdir %USERPROFILE%\.config\wezterm
 call rmdir %LOCALAPPDATA%\lazygit
 
 call mklink /D %LOCALAPPDATA%\nvim %~dp0..\config\nvim
-call mklink %USERPROFILE%\.wezterm.lua %~dp0..\config\wezterm\wezterm.lua
+call mklink /D %USERPROFILE%\.config\wezterm %~dp0..\config\wezterm
 call mklink /D %LOCALAPPDATA%\lazygit %~dp0..\config\lazygit
 
 pause
