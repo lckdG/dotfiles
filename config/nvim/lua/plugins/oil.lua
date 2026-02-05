f84b0faa1b7b98caca4208620ce234fec822f803
luckydoG_ <vkthanh1999@gmail.com>
2026-02-05 12:14:44 +0700

    feat(neovim): remap oil keymaps

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,9 @@
 return {
     'stevearc/oil.nvim',
     cmd = "Oil",
+    dependencies = {
+        'nvim-tree/nvim-web-devicons'
+    },
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
@@ -17,9 +20,21 @@ return {
         float = {
             border = "rounded",
             preview_split = "above",
+        },
+        use_default_keymaps = false,
+        keymaps = {
+            ["?"] = { "actions.show_help", mode = "n" },
+            ["<CR>"] = "actions.select",
+            ["p"] = "actions.preview",
+            ["<Esc>"] = { "actions.close", mode = "n" },
+            ["<C-r>"] = "actions.refresh",
+            ["-"] = { "actions.parent", mode = "n" },
+            ["_"] = { "actions.open_cwd", mode = "n" },
+            ["`"] = { "actions.cd", mode = "n" },
+            ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            ["<C-o>"] = "actions.open_external",
+            ["<C-h>"] = { "actions.toggle_hidden", mode = "n" },
+            ["<C-t>"] = { "actions.toggle_trash", mode = "n" },
         }
     },
-    dependencies = {
-        'nvim-tree/nvim-web-devicons'
-    }
 }
