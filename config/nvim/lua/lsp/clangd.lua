857ee75a304b837ea26b9e932e6fe78fe99ba62b
luckydoG_ <vkthanh1999@gmail.com>
2026-04-01 17:11:18 +0700

    feat(neovim): update clangd config

--- config/nvim/lua/lsp/clangd.lua
+++ config/nvim/lua/lsp/clangd.lua
@@ -6,7 +6,11 @@ local cmd = vim.fn.has("win32") == 1 and {
 
 table.insert(cmd, "--background-index")
 table.insert(cmd, "--clang-tidy")
+table.insert(cmd, "--header-insertion=iwyu")
 
 return {
     cmd = cmd,
+    init_options = {
+        fallbackFlags = { '-std=c++17' },
+    },
 }
