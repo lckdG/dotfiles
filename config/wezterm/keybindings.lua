5a06607ebfdde91e7646d193b888e9daadc1e8b7
luckydoG_ <vkthanh1999@gmail.com>
2026-04-02 11:10:09 +0700

    feat(weztermm): remove active tab keymaps

--- config/wezterm/keybindings.lua
+++ config/wezterm/keybindings.lua
@@ -48,10 +48,10 @@ function M.setup_keys(config)
         { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo "Clipboard" },
         { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom "Clipboard" },
 
-        { key = "]", mods = "CTRL", action = act.ActivateTabRelative(1) },
-        { key = "[", mods = "CTRL", action = act.ActivateTabRelative(-1) },
-        { key = "}", mods = "CTRL|SHIFT", action = act.MoveTabRelative(1) },
-        { key = "{", mods = "CTRL|SHIFT", action = act.MoveTabRelative(-1) },
+        { key = "}", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(1) },
+        { key = "{", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
+        -- { key = "}", mods = "CTRL|SHIFT", action = act.MoveTabRelative(1) },
+        -- { key = "{", mods = "CTRL|SHIFT", action = act.MoveTabRelative(-1) },
         { key = "]", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(1)},
         { key = "[", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(-1)},
 
