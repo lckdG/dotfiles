d57317f6954e2fe9ac115e64cc18c99d2ee625ad
luckydoG_ <vkthanh1999@gmail.com>
2026-06-12 15:14:51 +0700

    feat(neovim): update pyright to search for executables

--- config/nvim/lua/lsp/pyright.lua
+++ config/nvim/lua/lsp/pyright.lua
@@ -1,3 +1,19 @@
-return {
+local mason_path = vim.fn.stdpath('data') .. '/mason/bin/'
 
-}
+local bin = nil
+for _, name in ipairs({ 'basedpyright-langserver', 'pyright-langserver' }) do
+    local mason_bin = mason_path .. name
+    if vim.fn.executable(mason_bin) == 1 then
+        bin = mason_bin
+        break
+    end
+
+    local sys_bin = vim.fn.exepath(name)
+    if sys_bin ~= '' then
+        bin = sys_bin
+    end
+end
+
+return bin and {
+    cmd = { bin, '--stdio' }
+} or {}
