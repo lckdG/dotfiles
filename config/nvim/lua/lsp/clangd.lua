3efaef55880347bed6ad7c1b2d5a6bd68565e197
luckydoG_ <vkthanh1999@gmail.com>
2026-06-25 13:57:06 +0700

    feat(neovim): no need custom clangd

--- config/nvim/lua/lsp/clangd.lua
+++ config/nvim/lua/lsp/clangd.lua
@@ -1,15 +1,10 @@
-local cmd = vim.fn.has("win32") == 1 and {
-    vim.fn.expand("~") .. "/scoop/apps/clangd/current/bin/clangd"
-} or {
-    "clangd"
-}
-
-table.insert(cmd, "--background-index")
-table.insert(cmd, "--clang-tidy")
-table.insert(cmd, "--header-insertion=iwyu")
-
 return {
-    cmd = cmd,
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+    },
     init_options = {
         fallbackFlags = { '-std=c++17' },
     },
